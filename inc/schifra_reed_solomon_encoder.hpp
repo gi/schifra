3adec24b3d4cec8acb96383c257c04b275dd09c7
Zach Gianos <zach.gianos@gmail.com>
2016-12-01 07:13:36 -0800

    Spelling: rename reed_solomon_trait

--- inc/schifra_reed_solomon_encoder.hpp
+++ inc/schifra_reed_solomon_encoder.hpp
@@ -42,7 +42,7 @@ namespace schifra
       {
       public:
 
-         typedef traits::reed_solomon_triat<code_length,fec_length,data_length> trait;
+         typedef traits::reed_solomon_trait<code_length,fec_length,data_length> trait;
          typedef block<code_length,fec_length> block_type;
 
          encoder(const galois::field& gfield, const galois::field_polynomial& generator)
@@ -132,7 +132,7 @@ namespace schifra
       {
       public:
 
-         typedef traits::reed_solomon_triat<code_length,fec_length,data_length> trait;
+         typedef traits::reed_solomon_trait<code_length,fec_length,data_length> trait;
          typedef block<code_length,fec_length> block_type;
 
          shortened_encoder(const galois::field& gfield,
